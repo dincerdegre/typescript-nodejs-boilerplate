80637df8d3932125980728f84c54b0db60b1b020
Dincer Degre <johndoe@example.com>
2024-02-16 13:01:15 +0300

    add express functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("here");
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: ["*"],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: "Welcome",
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is Running PORT: "+ port);
+});
